96ccb67c54ce30fca31a828cae809da4d4d05b5b
MaxBill <maxbill1993@163.com>
2020-02-19 12:06:16 +0800

    提交在线聊天库

--- blog/js/daovoice.js
+++ blog/js/daovoice.js
@@ -1,1 +1,21 @@
-123.js
+(function (i, s, o, g, r, a, m) {
+    i["DaoVoiceObject"] = r;
+    i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    a.charset = "utf-8";
+    m.parentNode.insertBefore(a, m)
+})(window, document, "script", ('https:' == document.location.protocol ? 'https:' : 'http:') + "//widget.daovoice.io/widget/7c624f76.js", "daovoice");
+
+daovoice('init', {
+    app_id: "7c624f76",
+    user_id: "NO_89757",
+    email: "maxbill1993@163.com",
+    name: "MaxBill",
+    signed_up: 1449821660
+});
+
+daovoice('update');
